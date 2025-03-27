4c81c974c7182296b8f6b56d14e7cc33eee53565
Adedapo Adeyemi <adedapo025@gmail.com>
2025-03-27 18:31:38 +0100

    initiated business logic for signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from "argon"
+
 
 @Injectable()
 export class AuthService{
     constructor(private prisma: PrismaService){}
-    signup(){
-        return {msg: "I have signed up"}
+    async signup(dto: AuthDto){
+        // generate password hash
+        const hash = await argon.hash(dto.password);
+        // save user in the DB
+
+        // return user
     }
 
     login(){
