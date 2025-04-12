3acae285ae0f157988fa346e9fd49136beffb1a8
Adedapo Adeyemi <adedapo025@gmail.com>
2025-04-12 14:44:41 +0100

    added implementation for validate function in JwtStrategy

--- src/auth/strategy/auth.strategy.ts
+++ src/auth/strategy/auth.strategy.ts
@@ -1,11 +1,12 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
+import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt'){
-    constructor(private config: ConfigService){
+    constructor(config: ConfigService, private prisma: PrismaService){
         const jwtSecret = config.get<string>('JWT_SECRET');
         if (!jwtSecret) {
             throw new Error('Missing JWT_SECRET in environment variables')
@@ -15,7 +16,20 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt'){
             secretOrKey: jwtSecret
         });
     }
-    async validate(...args: any[]) {
-        
+    async validate(payload: {sub: string, email: string}) {
+        try {
+            const user = await this.prisma.user.findUnique({
+                where: {
+                    id: payload.sub
+                }
+            })
+            if (!user){
+                throw new UnauthorizedException("User not found!")
+            }
+            const { hashPassword, ...sanitizedUser } = user;
+            return sanitizedUser;
+        } catch (e){
+            throw e;
+        }        
     }
 } 
